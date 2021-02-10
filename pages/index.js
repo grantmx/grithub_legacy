2e0ec35327558308651339a02b4e136896146337
grantmx <support@sevengage.com>
2021-02-10 03:35:26 -0500

    copy and grammer updates

--- pages/index.js
+++ pages/index.js
@@ -53,8 +53,8 @@ export default function Home(){
 				<section className="container-xxl col-12 d-flex justify-content-center align-items-center p-5 flex-column" id="about">
 					<h1 className="fw-bold display-4 mb-4 text-center">Garden Route Innovation &amp; Technology Hub</h1>
 					<div className="text-center col-xl-9 col-12">
-						<p className="lead">We are a Non-Profit Company with a mission to be a catalyst providing training, programming and mentoring services in the Western Cape's Garden Route/Klien Karoo district business ecosystem.</p>
-						<p className="lead">Our duty is to empower schools, community members and small businesses to participate in the local supply chain through collaborative growth between the academic community, business, society and government using modern technology, innovation and STEM.</p>
+						<p className="lead">We are a Non-Profit Company with a mission to be a catalyst for providing training, programming and mentoring services in the Western Cape's Garden Route/Klien Karoo district business ecosystem.</p>
+						<p className="lead">Our duty is to empower community members and small businesses to actively participate in the local supply chain through collaborations with academia, business and government partners using modern technology and innovation.</p>
 					</div>
 				</section>
 
@@ -67,7 +67,7 @@ export default function Home(){
 							<div className="card shadow-sm h-100">
 								<Image src="/assets/workshop-thumb.jpg" width={500} height={333} className={home.cardTopImg} alt="Workshops and Programs" />
 								<div className="card-body">
-									<h3 className="card-title">Programs &amp; Workshops</h3>
+									<h3 className="card-title fs-4 fw-bold">Programs &amp; Workshops</h3>
 									<p className="card-text">Fueling a wholly inclusive economy empowering the Garden Route to meet the demands of the digital age. <a href="#programs-workshops">More &rsaquo;</a></p>
 								</div>
 							</div>
@@ -77,7 +77,7 @@ export default function Home(){
 							<div className="card shadow-sm h-100">
 								<Image src="/assets/incubator-thumb.jpg" width={500} height={333} className={home.cardTopImg} alt="Entrepreneurship Incubation" />
 								<div className="card-body">
-									<h3 className="card-title">SME and Entrepreneurship Incubation</h3>
+									<h3 className="card-title fs-4 fw-bold">SME and Entrepreneurship Incubation</h3>
 									<p className="card-text">Tailored workshops, networking, online learning and hands-on mentorship for accelerating business growth. <a href="#incubator">More &rsaquo;</a></p>
 								</div>
 							</div>
@@ -87,15 +87,15 @@ export default function Home(){
 							<div className="card shadow-sm h-100">
 								<Image src="/assets/office-space-thumb.jpg" width={500} height={333} className={home.cardTopImg} alt="About our coworking spaces"/>
 								<div className="card-body">
-									<h3 className="card-title">Coworking Spaces</h3>
-									<p className="card-text">Modern, sleek and creative spaces fostering collaboration and productivity taking your business higher. <a href="#coworking">More &rsaquo;</a></p>
+									<h3 className="card-title fs-4 fw-bold">Coworking Spaces</h3>
+									<p className="card-text">Modern, sleek and creative spaces fostering collaboration and productivity so that you can work without distraction. <a href="#coworking">More &rsaquo;</a></p>
 								</div>
 							</div>
 						</div>
 
 						<div className="d-md-flex d-xs-none d-xl-none col-xl-4 col-md-6 col-12 p-md-5 p-4 justify-content-center flex-column align-item-center">
-							<h3 className="display-6 fw-bold">We strive for wholistic change at every level of South Africa.</h3>
-							<p className="lead">Our core values of Collaboration, Equality, Responsibility, Entrepreneurial Ethic, Innovation and a Giving Spirit bind us and guide us.</p>
+							<h3 className="display-6 fw-bold">We strive for wholistic change at every level of the economy.</h3>
+							<p className="lead">Our core values of: Collaboration, Equality, Responsibility, Entrepreneurial Ethic, Innovation and a Giving Spirit not only bind us, but guide us.</p>
 						</div>
 
 					</div>
@@ -105,7 +105,7 @@ export default function Home(){
 				<section className="container-fluid d-flex justify-content-center align-items-center py-5 flex-column bg-info" id="incubator">
 					<h2 className="fw-bold display-5 mt-5 mb-4 text-center text-white">Small Business &amp; Entrepreneur Incubator</h2>
 					<div className="text-center col-xl-9 col-12 mb-5">
-						<p className="lead text-white ">Our Incubator is custom designed for the Small to Medium Enterprises to help them digitally grow their skills, refine or create a digital business model through Masterclasses, worships, events and direct coaching.</p>
+						<p className="lead text-white ">Our Incubator is custom designed for Small to Medium Enterprises to help grow digital skills, refine or create an online business model through masterclasses, workshops, events and direct coaching.</p>
 					</div>
 
 					<div className="col-12  d-flex justify-content-between flex-wrap">
@@ -113,8 +113,8 @@ export default function Home(){
 							<div className="card shadow h-100">
 								<Image src="/assets/power.svg" width={150} height={150} className={home.cardTopOverflow} alt="SME Coaching" />
 								<div className="card-body">
-									<h3 className="card-title fw-bold">SME Digital Transformation &amp; Coaching</h3>
-									<p className="card-text">The Small to Medium Enterprises in South Africa are the backbone of it's economy. SMEs have the ability to quickly adapt to new innovative tools and competencies to propel sector growth. Our goal is to coach and provide tools, information platforms to increase productivity and innovation.</p>
+									<h3 className="card-title fs-4 fw-bold">SME Digital Transformation &amp; Coaching</h3>
+									<p className="card-text">The Small to Medium Enterprises in South Africa are the backbone of its economy. SMEs have the ability to quickly adapt to new innovative tools and competencies to propel sector growth. Our goal is to coach and provide tools and information platforms to increase productivity and innovation.</p>
 									<a href="/papers/eBook-SMB-of-the-Future-FINAL-2019-07-29.pdf" target="_blank" rel="noopener" title="whitepaper on digital transformation">White Paper: 7 Trends Driving SMEs’ Digital Transformation &rsaquo;</a>
 								</div>
 							</div>
@@ -124,8 +124,8 @@ export default function Home(){
 							<div className="card shadow h-100">
 								<Image src="/assets/worker.svg" width={150} height={150} className={home.cardTopOverflow} alt="Digital Skills"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">Digital Skills &amp; Apprenticeships</h3>
-									<p className="card-text">Our Apprenticeships and Skills training will fill the immediate needs of employers in an ever increasing digital economy.  From CompTIA A+ and Mac Circuitry Certifications to various scripting Languages, Cloud Computing, Data Analytics and OS Certifications we will thoroughly equip the next digital workforce.</p>
+									<h3 className="card-title fs-4 fw-bold">Digital Skills &amp; Apprenticeships</h3>
+									<p className="card-text">Our Apprenticeships and Skills training will fill the immediate needs of employers in an ever increasing digital economy.  From CompTIA A+ and Mac Circuitry Certifications, to various scripting Languages, Cloud Computing, Data Analytics and OS Certifications we will thoroughly equip the next digital workforce.</p>
 									<a href="/papers/skills_boost_western_cape_-_summary_report.pdf" target="_blank" rel="noopener" title="whitepaper on digital skills jobs">White Paper: Western Cape Digital Skills Shared Agenda for Action &rsaquo;</a>
 								</div>
 							</div>
@@ -135,8 +135,8 @@ export default function Home(){
 							<div className="card shadow h-100">
 								<Image src="/assets/3d-printer.svg" width={150} height={150} className={home.cardTopOverflow} alt="FabLab Innovation"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">FabLab Innovation Centre</h3>
-									<p className="card-text">Disruptive Technologies like "Additive Manufacturing" or 3D Printing paired with the sharp removal of it's price barrier allows young entrepreneurs to enter previously closed markets. Our <a href="https://fabfoundation.org/" target="_blank" rel="noopener">FabLab</a> will assist in product development, rapid tooling and prototyping as well as learning new business models.</p>
+									<h3 className="card-title fs-4 fw-bold">FabLab Innovation Centre</h3>
+									<p className="card-text">Disruptive Technologies like "Additive Manufacturing" or 3D Printing, paired with the sharp removal of its price barrier, allows young entrepreneurs to enter previously closed markets. Our <a href="https://fabfoundation.org/" target="_blank" rel="noopener">FabLab</a> will assist in product development, rapid tooling and prototyping as well as learning new business models.</p>
 									<a href="/papers/1-s2.0-S0040162520313093-main.pdf" target="_blank" rel="noopener" title="whitepaper on 3d printing">White Paper: The Effect of 3D Printing Technologies on Entrepreneurship &rsaquo;</a>
 								</div>
 							</div>
@@ -167,8 +167,8 @@ export default function Home(){
 								<div className={`${ribbon.ribbon_top_right} ${ribbon.ribbon__green}`}><span>STEM</span></div>
 								<Image src="/assets/atom.svg" width={150} height={150} className={home.cardTopOverflow} alt="STEM"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">Science, Technology, Engineering and Mathematics (STEM)</h3>
-									<p className="card-text">We are a partner in science, technology and industrial learning to supplement the inadequate number of teachers skilled to educate in these areas.  We will promote STEM carriers, and teach short courses as well as create fun STEM based science events.</p>
+									<h3 className="card-title fs-4 fw-bold">Science, Technology, Engineering and Mathematics (STEM)</h3>
+									<p className="card-text">We are a partner in science, technology and industrial learning to supplement the inadequate number of teachers skilled to educate in these areas.  We will promote STEM careers, and teach short courses as well as create fun STEM based science events.</p>
 									<a href="/papers/1-s2.0-S2468227619307616-main.pdf" target="_blank" rel="noopener" title="whitepaper on stem in africa">White Paper: Engineering education, development and growth in Africa &rsaquo;</a>
 								</div>
 							</div>
@@ -179,8 +179,8 @@ export default function Home(){
 								<div className={`${ribbon.ribbon_top_right} ${ribbon.ribbon__green}`}><span>STEM</span></div>
 								<Image src="/assets/lego.svg" width={150} height={150} className={home.cardTopOverflow} alt="LEGO STEM"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">LEGO&reg; STEM Learning</h3>
-									<p className="card-text">The LEGO&reg; Foundation released a report stating that STEM learning through play holistically enhances a learners social, emotional, physical and creative skills. Through <a href="https://education.lego.com/en-us/customer-stories" target="_blank">LEGO&reg; STEM</a> our goal is to eliminate the idea of "Hard" math and science through fun and curious play using LEGO&reg; STEM Toys in Grade R-12 schools.</p>
+									<h3 className="card-title fs-4 fw-bold">LEGO&reg; STEM Learning</h3>
+									<p className="card-text">The LEGO&reg; Foundation released a report stating that STEM learning through play holistically enhances a learner's social, emotional, physical and creative skills. Through <a href="https://education.lego.com/en-us/customer-stories" target="_blank">LEGO&reg; STEM</a> our goal is to eliminate the idea of "Hard" math and science through fun and curious play using LEGO&reg; STEM Toys in Grade R-12 schools.</p>
 									<a href="/papers/le_learning_through_play_whitepaper_digital_pdf.pdf" target="_blank" rel="noopener" className="card-link" title="whitepaper on lego education">White Paper: LEGO&reg; Education - The unrealized potential of learning through play at school &rsaquo;</a>
 								</div>
 							</div>
@@ -192,8 +192,8 @@ export default function Home(){
 							<div className={`${ribbon.ribbon_top_right} ${ribbon.ribbon__purple}`}><span>Workshop</span></div>
 							<Image src="/assets/programmer.svg" width={150} height={150} className={home.cardTopOverflow} alt="Hackathons"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">Code &amp; Enterprise Hackathons</h3>
-									<p className="card-text">Hackathons help break through the barriers of a stale "business as usual" culture into creative disruption and intense innovation. Our 1-3 day Hackathon events will not only solve technical problems, and deliver new innovative software and hardware, but will accelerate organizational digitization strategies and customer processes.</p>
+									<h3 className="card-title fs-4 fw-bold">Code &amp; Enterprise Hackathons</h3>
+									<p className="card-text">Hackathons help break through the barriers of a stale "business as usual" culture into creative disruption and intense innovation. Our 1-3 day Hackathon events will not only solve technical problems, and deliver new innovative software and hardware, but will accelerate organizational digitization strategies and improve customer processes.</p>
 									<a href="/papers/Demystifying_the_hackathon.pdf" target="_blank" rel="noopener" title="whitepaper on hackathons">White Paper: Demystifying The Hackathon &rsaquo;</a>
 								</div>
 							</div>
@@ -205,8 +205,8 @@ export default function Home(){
 								<div className={`${ribbon.ribbon_top_right}`}><span>Program</span></div>
 								<Image src="/assets/healthcare.svg" width={150} height={150} className={home.cardTopOverflow} alt="Public Health"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">Public Health Technology</h3>
-									<p className="card-text">Our aim is to significantly contribute to the digitization of health care in South Africa. We will produce tools, software and data analytics to improve and enhance awareness of access to public healthcare which will ultimately improve the health outcomes of the Garden Route community.</p>
+									<h3 className="card-title fs-4 fw-bold">Public Health Technology</h3>
+									<p className="card-text">Our aim is to significantly incorporate modern technology into South Africa's health sector. We will produce tools, software and data analytics to improve and enhance awareness of access to public healthcare which will ultimately improve the health outcomes in the Garden Route community.</p>
 									<a href="/papers/Materilie345_digital_health_africa.pdf" target="_blank" rel="noopener" title="whitepaper on digital health ecosystem">White Paper: Digital Health Ecosystem for African Countries &rsaquo;</a>
 								</div>
 							</div>
@@ -218,7 +218,7 @@ export default function Home(){
 								<div className={`${ribbon.ribbon_top_right}`}><span>Program</span></div>
 								<Image src="/assets/game-pad.svg" width={150} height={150} className={home.cardTopOverflow} alt="Roblox Proggrams"/>
 								<div className="card-body">
-									<h3 className="card-title fw-bold">Roblox&reg; Game Engineering</h3>
+									<h3 className="card-title fs-4 fw-bold">Roblox&reg; Game Engineering</h3>
 									<p className="card-text">Gamification is a powerful vehicle for learning. Our goal is to have introductory workshops and courses on development and design using the popular <a href="https://education.roblox.com/en-us/" target="_blank">Roblox&reg;</a> platform amongst others. We will use our Gaming Center to teach students of all ages computer science, digital citizenship, entrepreneurship, competition and fair play.</p>
 									<a href="/papers/1-s2.0-S1071581920300987-main.pdf" target="_blank" title="whitepaper on gamification" rel="noopener">White Paper: The Effect of Challenge-Based Gamification on Learning &rsaquo;</a>
 								</div>
@@ -230,7 +230,7 @@ export default function Home(){
 								<div className={`${ribbon.ribbon_top_right}`}><span>Program</span></div>
 								<Image src="/assets/tractor.svg" width={150} height={150} className={home.cardTopOverflow} alt="Agricultural Technology"/>
 								<div className="card-body">
-									<h3 className="card-title">Agricultural Technology</h3>
+									<h3 className="card-title fs-4 fw-bold">Agricultural Technology</h3>
 									<p className="card-text">Agriculture 4.0 uses operational technology such as robots, Internet of Things (IoT) and GPS. This technology allows farms to be more profitable, efficient, safer, and more environmentally friendly. Through AgriTech we aim to innovate and assist in modernizing the Garden Route's farms, thereby broadening their supply chain and viability.</p>
 									<a href="/papers/1-s2.0-S2211912419301804-main.pdf" target="_blank" rel="noopener" title="whitepaper on agricultural technology">White Paper: The Game-Changing Technologies of Agriculture 4.0 &rsaquo;</a>
 								</div>
@@ -244,7 +244,7 @@ export default function Home(){
 					<div className="col-12 d-flex align-items-center flex-column">
 						<h2 className="fw-bold display-5 mt-5 mb-4 text-center text-white">Coworking Office &amp; Event Spaces</h2>
 						<div className="col-md-9 col-12 mb-5">
-							<p className="lead text-white text-center">Fully furnished and modern coworking and meeting spaces to inspire creativity, promote cluster collaboration.  Our spaces will meet all of your needs ensuring your and your business' productivity.</p>
+							<p className="lead text-white text-center">Fully furnished and modern coworking and meeting spaces to inspire creativity and promote cluster collaboration.  Our spaces will provide a productive and secure work environment giving you the opportunity to network with like minded people.</p>
 						</div>
 
 						<div className="col-12 d-flex flex-wrap">
